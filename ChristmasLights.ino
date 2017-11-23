2eaa763bd5a2a4d092630b606051701cb848b3c1
Peter Grace <pete.grace@gmail.com>
2017-11-23 14:26:38 -0500

    Revise code a bit to make it generic so I can run it on many different strips at once.

--- ChristmasLights.ino
+++ ChristmasLights.ino
@@ -15,7 +15,6 @@
 
 #define BOARD WS2812B
 #define PIN 5
-#define NUM_LEDS 123
 #define COLOR_ORDER GRB
 
 #define FADE_VALUE 10
@@ -35,13 +34,21 @@ unsigned long _uptime=0;
 
 int status_screen_interval=10000;
 
+#define VALID_VER 124
+
+struct cl_config {
+  int mode;
+  int bpm;
+  int brightness;
+  int num_leds;
+} config;
 
 const char newicicle[]="Starting new icicle";
 const char new_begin[]="Beginning new icicle timer";
 const char new_animate[]="Beginning velocity timer";
 const char icicledelay[]="icicle was due to start, but we're still animating.  Resetting timer.";
 const char trymsg[]="Trying to connect to wifi";
-CRGB leds[NUM_LEDS];
+CRGB leds[1024];
 CRGB endclr, midclr;
 
 WiFiUDP UDP;
@@ -55,10 +62,8 @@ int newtime=0;
 
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
-//mode settings
-int mode_setting=0;
-int global_bpm=20;
-long bpm_millis=60000/global_bpm;
+
+long bpm_millis=0;
 
 // voob settings
 int voob_color=0;
@@ -79,9 +84,7 @@ int swipe_sin2 = 0;
 
 // the addresses we will use to store integers in.
 #define ADDR_VALID 0
-#define ADDR_BPM 1
-#define ADDR_BRIGHT 2
-#define ADDR_MODE 3
+#define ADDR_CONFIG 1
 #define EEPROM_SIZE 64
 uint8_t gHue = 0;
 
@@ -117,49 +120,45 @@ void setup() {
   // put your setup code here, to run once:
  EEPROM.begin(EEPROM_SIZE);
  Serial.begin(74880);
+ initialize_wifi(); 
  delay(3000); 
+ load_eeprom_values(); 
+ 
  MDNS.begin(WiFi.hostname().c_str());
  httpUpdater.setup(&httpServer);
  httpServer.on("/lights", HTTP_POST, process_post);
  httpServer.on("/reboot/please", HTTP_POST, [](){httpServer.send ( 200, "text/json", "{\"done\":true}" );ESP.restart();});
- httpServer.on("/save/eeprom", HTTP_POST, [](){httpServer.send ( 200, "text/json", "{\"done\":true}" );cl_write_eeprom();});
+ httpServer.on("/eeprom/write", HTTP_POST, [](){httpServer.send ( 200, "text/json", "{\"done\":true}" );cl_write_eeprom();});
+ httpServer.on("/eeprom/read", HTTP_POST, [](){httpServer.send ( 200, "text/json", "{\"done\":true}" );validate_eeprom();});
  
  httpServer.begin();
  MDNS.addService("http", "tcp", 80);
  
 
- FastLED.addLeds<BOARD, PIN, COLOR_ORDER>(leds, NUM_LEDS);
+ FastLED.addLeds<BOARD, PIN, COLOR_ORDER>(leds, config.num_leds);
  set_brightness(255);
- initialize_wifi();
  char initialized[MAX_MSG_SIZE];
  sprintf(initialized,"Initialized.  Frame rate dictates minimum refresh rate of: %d", min_millis);
  debugmsg(initialized);
  randomSeed(analogRead(0));
- load_eeprom_values();
 }
 
 void cl_write_eeprom()
 {
   char eeprommsg[MAX_MSG_SIZE];
-  EEPROM.write(ADDR_VALID, 122);
-  EEPROM.write(ADDR_MODE, mode_setting);
-  EEPROM.write(ADDR_BPM,global_bpm);
-  EEPROM.write(ADDR_BRIGHT, global_brightness);
+  EEPROM.write(ADDR_VALID, VALID_VER);
+  EEPROM.put(ADDR_CONFIG, config);
   EEPROM.commit();
-  sprintf(eeprommsg, "Saved to eeprom: m:%d, bpm:%d, bright:%d\n",mode_setting, global_bpm, global_brightness);
+  sprintf(eeprommsg, "Saved to eeprom: m:%d, bpm:%d, bright:%d,leds:%d\n",config.mode, config.bpm, config.brightness,config.num_leds);
   debugmsg(eeprommsg);
   validate_eeprom();
 }
 
 void validate_eeprom()
 {
-  int _valid, _bpm, _bright, _mode=0;
   char eeprommsg[MAX_MSG_SIZE];
-  _valid = EEPROM.read(ADDR_VALID);
-  _bpm = EEPROM.read(ADDR_BPM);
-  _bright = EEPROM.read(ADDR_BRIGHT);
-  _mode = EEPROM.read(ADDR_MODE);
-  sprintf(eeprommsg, "v:%d, bpm:%d, mode:%d, bright:%d",_valid,_bpm,_mode,_bright);
+  EEPROM.get(ADDR_CONFIG, config);
+  sprintf(eeprommsg, "Read from eeprom: m:%d, bpm:%d, bright:%d, leds:%d\n",config.mode, config.bpm, config.brightness,config.num_leds);
   debugmsg(eeprommsg);
 }
 
@@ -170,31 +169,26 @@ void load_eeprom_values()
   char eeprommsg[MAX_MSG_SIZE];
   sprintf(eeprommsg,"Valid came back as %d\n", _valid);    
   debugmsg(eeprommsg);
-  if (_valid == 122)
+  if (_valid == VALID_VER)
   {
-    _bpm = EEPROM.read(ADDR_BPM);
-    _bright = EEPROM.read(ADDR_BRIGHT);
-    _mode = EEPROM.read(ADDR_MODE);
+    EEPROM.get(ADDR_CONFIG, config);    
     sprintf(eeprommsg, "EEPROM was valid\n");
     debugmsg(eeprommsg);
   }
   else
   {
-    _bpm = 60;
-    _bright = 255;
-    _mode = 3;
-    EEPROM.write(ADDR_VALID, 122);
-    EEPROM.write(ADDR_MODE, _mode);
-    EEPROM.write(ADDR_BPM,_bpm);
-    EEPROM.write(ADDR_BRIGHT, _bright);
-    EEPROM.commit();
+    config.bpm = 60;
+    config.brightness = 255;
+    config.mode = 3;
+    config.num_leds = 123;
+    cl_write_eeprom();
     sprintf(eeprommsg, "Wrote defaults to eeprom\n",_mode,_bpm,_bright);
     debugmsg(eeprommsg);
 
   }
-  set_bpm(_bpm);
-  set_brightness(_bright);
-  mode_setting = _mode;
+  set_bpm(config.bpm);
+  set_brightness(config.brightness);
+  config.mode = config.mode;
 
 }
 
@@ -212,26 +206,26 @@ void random_colors()
 void confetti_mode() 
 {
   // random colored speckles that blink in and fade smoothly
-  fadeToBlackBy( leds, NUM_LEDS, 10);
-  int pos = random16(NUM_LEDS);
+  fadeToBlackBy( leds, config.num_leds, 10);
+  int pos = random16(config.num_leds);
   leds[pos] += CHSV( random8(255), 200, 255);
 }
 
 void rainbow_solid()
 {
-  fill_rainbow(leds, NUM_LEDS, 0, 7);
+  fill_rainbow(leds, config.num_leds, 0, 7);
 }
 
 void rainbow_bpm()
 {
-  uint8_t beatA = beatsin8(global_bpm, 0, 255);
-  fill_rainbow(leds, NUM_LEDS, beatA, 7);
+  uint8_t beatA = beatsin8(config.bpm, 0, 255);
+  fill_rainbow(leds, config.num_leds, beatA, 7);
 }
 
 void murica_chase()
 {
-  fadeToBlackBy( leds, NUM_LEDS, 10);
-  uint8_t pos=beatsin8(global_bpm,1,NUM_LEDS-1);
+  fadeToBlackBy( leds, config.num_leds, 10);
+  uint8_t pos=beatsin8(config.bpm,1,config.num_leds-1);
   leds[pos-1]=CRGB(CRGB::Red);
   leds[pos]=CRGB(CRGB::White);
   leds[pos+1]=CRGB(CRGB::Blue);
@@ -244,22 +238,22 @@ void solid_mode()
     {
       case 0: {
                 //red hue
-                fill_solid(leds, NUM_LEDS, CHSV(0,255,bright));
+                fill_solid(leds, config.num_leds, CHSV(0,255,bright));
                 break;
               }
       case 1: {
                 //green hue
-                fill_solid(leds, NUM_LEDS, CHSV(96,255,bright));
+                fill_solid(leds, config.num_leds, CHSV(96,255,bright));
                 break;
               }
       case 2: {
                 //blue hue
-                fill_solid(leds, NUM_LEDS, CHSV(160,255,bright));
+                fill_solid(leds, config.num_leds, CHSV(160,255,bright));
                 break;
               }
       case 3: {
                 //orange hue
-                fill_solid(leds, NUM_LEDS, CHSV(32,255,bright));
+                fill_solid(leds, config.num_leds, CHSV(32,255,bright));
                 break;
               }
       }
@@ -268,27 +262,27 @@ void solid_mode()
 
 void voob_mode()
 {
-    uint8_t bright = beatsin8(global_bpm,0,255);
+    uint8_t bright = beatsin8(config.bpm,0,255);
     switch (voob_color)
     {
       case 0: {
                 //red hue
-                fill_solid(leds, NUM_LEDS, CHSV(0,255,bright));
+                fill_solid(leds, config.num_leds, CHSV(0,255,bright));
                 break;
               }
       case 1: {
                 //green hue
-                fill_solid(leds, NUM_LEDS, CHSV(96,255,bright));
+                fill_solid(leds, config.num_leds, CHSV(96,255,bright));
                 break;
               }
       case 2: {
                 //blue hue
-                fill_solid(leds, NUM_LEDS, CHSV(160,255,bright));
+                fill_solid(leds, config.num_leds, CHSV(160,255,bright));
                 break;
               }
       case 3: {
                 //orange hue
-                fill_solid(leds, NUM_LEDS, CHSV(32,255,bright));
+                fill_solid(leds, config.num_leds, CHSV(32,255,bright));
                 break;
               }
       }
@@ -299,14 +293,14 @@ void doIcicle()
 {
   if (icicle_animate)
   {
-    fadeToBlackBy( leds, NUM_LEDS, 100);  
+    fadeToBlackBy( leds, config.num_leds, 100);  
     leds[icicle_pos] = CHSV(160,128,255);
     icicle_pos++;
-    if (icicle_pos > NUM_LEDS+1)
+    if (icicle_pos > config.num_leds+1)
     {
       icicle_animate = false;
       icicle_pos=0;
-      fill_solid(leds, NUM_LEDS, CRGB::Black);
+      fill_solid(leds, config.num_leds, CRGB::Black);
     }
   }
 }
@@ -360,20 +354,20 @@ void icicle_mode()
 
 void red_voob()
 {
-  uint8_t bright = beatsin8(global_bpm,0,255);
-  fill_solid(leds, NUM_LEDS, CHSV(0,255,bright));
+  uint8_t bright = beatsin8(config.bpm,0,255);
+  fill_solid(leds, config.num_leds, CHSV(0,255,bright));
 }
 
   
 
 void black()
 {
-  fadeToBlackBy(leds, NUM_LEDS, 100);
+  fadeToBlackBy(leds, config.num_leds, 100);
 }
 
 void modeSelection()
 {
-  switch (mode_setting)
+  switch (config.mode)
   {
     case 1: {
                 // choose red, green, blue or orange to voob
@@ -476,34 +470,34 @@ void modeSelection()
 
 void swipe_redgreen()
 {
-    swipe_sin1=beatsin8(global_bpm, 0, NUM_LEDS,0,0);
+    swipe_sin1=beatsin8(config.bpm, 0, config.num_leds,0,0);
     fill_gradient_RGB(leds, 0, CRGB::Green, swipe_sin1, CRGB::Red);
-    fill_gradient_RGB(leds, swipe_sin1, CRGB::Red, NUM_LEDS, CRGB::Green);
+    fill_gradient_RGB(leds, swipe_sin1, CRGB::Red, config.num_leds, CRGB::Green);
 }
 
 void swipe_redblue()
 {
-    swipe_sin1=beatsin8(global_bpm, 0, NUM_LEDS,0,0);
+    swipe_sin1=beatsin8(config.bpm, 0, config.num_leds,0,0);
     fill_gradient_RGB(leds, 0, CRGB::Blue, swipe_sin1, CRGB::Red);
-    fill_gradient_RGB(leds, swipe_sin1, CRGB::Red, NUM_LEDS, CRGB::Blue);
+    fill_gradient_RGB(leds, swipe_sin1, CRGB::Red, config.num_leds, CRGB::Blue);
 }
 
 void swipe_bluegreen()
 {
-    swipe_sin1=beatsin8(global_bpm, 0, NUM_LEDS,0,0);
+    swipe_sin1=beatsin8(config.bpm, 0, config.num_leds,0,0);
     fill_gradient_RGB(leds, 0, CRGB::Blue, swipe_sin1, CRGB::Green);
-    fill_gradient_RGB(leds, swipe_sin1, CRGB::Green, NUM_LEDS, CRGB::Blue);
+    fill_gradient_RGB(leds, swipe_sin1, CRGB::Green, config.num_leds, CRGB::Blue);
 }
 
 
 void gradient_mode()
 {
   
-  uint8_t speed = beatsin8(global_bpm,0,NUM_LEDS);
+  uint8_t speed = beatsin8(config.bpm,0,config.num_leds);
   endclr = blend(CRGB::Red, CRGB::Green, speed);
   midclr = blend(CRGB::Green, CRGB::Blue, speed);
-  fill_gradient_RGB(leds, 0, endclr, NUM_LEDS/2, midclr);
-  fill_gradient_RGB(leds, NUM_LEDS/2+1, midclr, NUM_LEDS, endclr);  
+  fill_gradient_RGB(leds, 0, endclr, config.num_leds/2, midclr);
+  fill_gradient_RGB(leds, config.num_leds/2+1, midclr, config.num_leds, endclr);  
 }
 
 
@@ -512,7 +506,7 @@ void loop() {
   unsigned long now=ntp.getEpochTime();
   if ((pause_till >=0) && (now <pause_till))
   {
-    fadeToBlackBy(leds, NUM_LEDS,100);
+    fadeToBlackBy(leds, config.num_leds,100);
   } else if ((pause_till <= 0) || (now >=pause_till))
   {
     modeSelection();    
@@ -533,10 +527,10 @@ void set_bpm(int _bpm)
 {
   char bpmmsg[MAX_MSG_SIZE];
 
-  global_bpm = _bpm;
-  bpm_millis = 60000/global_bpm;
+  config.bpm = _bpm;
+  bpm_millis = 60000/config.bpm;
 
-  sprintf(bpmmsg, "BPM set to %d (millis: %d)", global_bpm, bpm_millis);
+  sprintf(bpmmsg, "BPM set to %d (millis: %d)", config.bpm, bpm_millis);
   debugmsg(bpmmsg);
 }
 
@@ -548,7 +542,7 @@ void process_post()
   char msg[MAX_MSG_SIZE];
   JsonObject& input = jsonBuffer.parseObject(httpServer.arg("plain"));
   if (input["mode"])
-    mode_setting=input["mode"];
+    config.mode=input["mode"];
   if (input["sync_start"])
     pause_till=atol(input["sync_start"]);
   if (input["brightness"])
@@ -589,7 +583,13 @@ void process_post()
   }
   if (input["icicle_wait_max"])
     icicle_wait_min=input["icicle_wait_max"];      
-  httpServer.send ( 200, "text/json", "{\"done\":true}" );    
+  if (input["num_leds"])
+    {
+      sprintf(msg,"Received %s from packet, writing %d to config.\n",input["num_leds"], atoi(input["num_leds"]));
+      debugmsg(msg);
+      config.num_leds = atoi(input["num_leds"]);
+    }
+    httpServer.send ( 200, "text/json", "{\"done\":true}" );    
 }
 
 void set_brightness(uint8_t brightness)
--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 curl -XPOST -F 'update=@build/ChristmasLights.ino.bin' http://10.65.3.249/update
+curl -XPOST -F 'update=@build/ChristmasLights.ino.bin' http://10.65.3.239/update
